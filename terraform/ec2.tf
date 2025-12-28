7589e79c9db41fa1bca6e4e973c46402d688fdbd
Tejas-Pokale <tejaspokale9204@gmail.com>
2025-12-28 14:29:52 +0530

    Updated kubernetes manifests

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "terra-automate-key"
-  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
+  public_key = file("terra-automate-key.pub")
 }
 
 resource "aws_default_vpc" "default" {
@@ -11,19 +11,12 @@ resource "aws_security_group" "allow_user_to_connect" {
   name        = "allow TLS"
   description = "Allow user to connect"
   vpc_id      = aws_default_vpc.default.id
+
   ingress {
     description = "port 22 allow"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = " allow all outgoing traffic "
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -43,18 +36,26 @@ resource "aws_security_group" "allow_user_to_connect" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    description = " allow all outgoing traffic "
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
-    Name = "mysecurity"
+    Name = "${var.tag_name}-mysecurity"
   }
 }
 
-resource "aws_instance" "testinstance" {
+resource aws_instance eks_mega_project_master{
   ami             = var.ami_id
   instance_type   = var.instance_type
   key_name        = aws_key_pair.deployer.key_name
   security_groups = [aws_security_group.allow_user_to_connect.name]
   tags = {
-    Name = "Automate"
+    Name = "${var.tag_name}-Automate"
   }
   root_block_device {
     volume_size = 30 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,19 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  default     = "ap-south-1"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "ami-02b8269d5e85954ef"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
-  default     = "t2.large"
+  default     = "m7i-flex.large"
+}
+
+variable "tag_name" {
+  description = "The default tag name to use for this project"
+  default = "eks-mega-project"
 }
