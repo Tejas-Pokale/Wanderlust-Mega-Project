7a2e41fe2771c5567c0ca42f545161fb9f50410e
Tejas-Pokale <tejaspokale9204@gmail.com>
2025-12-31 12:52:39 +0530

    Updated backend service type to LoadBalancer and  service port to 80

--- Automations/updatebackendnew.sh
+++ Automations/updatebackendnew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the Instance ID and path to the .env file
-INSTANCE_ID="i-04c25450831524e97"
+INSTANCE_ID="i-0d7bb07db9c705104"
 
 # Retrieve the public IP address of the specified EC2 instance
 ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
