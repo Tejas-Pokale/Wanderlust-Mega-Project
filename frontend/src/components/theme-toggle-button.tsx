8a0df8d4b844714517aeee2419332c8c3cf103ce
Tejas-Pokale <tejaspokale9204@gmail.com>
2025-12-31 15:08:01 +0530

    Updated Image Urls in Frontend

--- frontend/src/components/theme-toggle-button.tsx
+++ frontend/src/components/theme-toggle-button.tsx
@@ -1,6 +1,6 @@
 import { useLayoutEffect, useState } from 'react';
-import Sun from '../../public/svg/sun.svg';
-import Moon from '../../public/svg/moon.svg';
+import Sun from '../../public/assets/svg/sun.svg';
+import Moon from '../../public/assets/svg/moon.svg';
 function ThemeToggle() {
   const [isDarkTheme, setIsDarkTheme] = useState<boolean | null>(null);
   const toggleTheme = () => {
